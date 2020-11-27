8d9b1e5a06ffe75de31f6865543e4c6431a60bb3
sshingh67 <singhsharandeep676@gmail.com>
2020-11-26 23:32:42 -0800

    400 error message

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -24,10 +24,16 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
+    const exisitingUser = await User.findOne({ email });
 
-    res.send({});
+    if (exisitingUser) {
+      throw new BadRequestError("Email in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
