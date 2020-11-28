b96661f7e319bd3bdbee8cd037449b004dd028b8
sshingh67 <singhsharandeep676@gmail.com>
2020-11-27 20:53:54 -0800

    class for password hashing

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -1,7 +1,7 @@
-import { scrpyt, randomBytes } from "crytpo";
+import { scrypt, randomBytes } from "crypto";
 import { promisify } from "util";
 
-const scryptAsync = promisify(scrpyt);
+const scryptAsync = promisify(scrypt);
 
 export class Password {
   static async toHash(password: string) {
@@ -11,5 +11,10 @@ export class Password {
     return `${buf.toString("hex")}.${salt}`;
   }
 
-  static compare(storedPassword: string, suppliedPassword: string) {}
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split("");
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buf.toString("hex") === hashedPassword;
+  }
 }
