2bb52fb7e5de4eb3bc51bb05c565a1921238d283
unknown <singhsharandeep676@gmail.com>
2020-12-21 20:19:30 -0500

    root route

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are signed out</h1>;
+};
+
+LandingPage.getInitialProps = async (context) => {
+  console.log("LandingPage");
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
+};
+
+export default LandingPage;
