ac5f2c5073d3968b81e1db39984e15ce93e47829
unknown <singhsharandeep676@gmail.com>
2020-12-26 20:59:20 -0500

    added mock up nats-wrapper for creating a fake nats publisher client for testing event publishing

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -12,6 +12,8 @@ declare global {
   }
 }
 
+jest.mock("../nats-wrapper");
+
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = "asfd";
@@ -25,6 +27,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+  jest.clearAllMocks();
   const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
