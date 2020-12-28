c46590a0c9056c9fc3cd5fe874c7d537bb0356be
unknown <singhsharandeep676@gmail.com>
2020-12-28 03:40:22 -0500

    added test case for testing event publishing

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,8 +2,9 @@ import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import { Order, OrderStatus } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
 
-it("mars an order as cancelled", async () => {
+it("marks an order as cancelled", async () => {
   // Create a ticket with Ticket Model
   const ticket = Ticket.build({
     title: "concert",
@@ -32,4 +33,28 @@ it("mars an order as cancelled", async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emits a order cancelled event");
+it("emits a order cancelled event", async () => {
+  // Create a ticket with Ticket Model
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  //make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
