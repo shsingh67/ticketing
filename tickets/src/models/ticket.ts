2790fe42be6fc835edaeca115933e456161e164b
unknown <singhsharandeep676@gmail.com>
2020-12-28 22:38:54 -0500

    records saved to mongodb will now have a version number

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 //Properties that required to build a new Ticket
 interface TicketAttrs {
@@ -12,6 +13,7 @@ interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
   userId: string;
+  version: number;
 }
 
 // Properties tied to the Model
@@ -43,6 +45,8 @@ const ticketSchema = new mongoose.Schema(
     },
   }
 );
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
